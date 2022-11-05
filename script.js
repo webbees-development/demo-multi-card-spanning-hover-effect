639735417bec838e3dcf9b0aaac977df225290f5
stephan steiner <dev@webbees.at>
2022-11-05 16:57:28 +0100

    Initial Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("cards").onmousemove = (e) => {
+    for (const card of document.querySelectorAll(".card")) {
+        const rect = card.getBoundingClientRect(),
+        x = e.clientX - rect.left,
+        y = e.clientY - rect.top;
+
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+    }
+}
